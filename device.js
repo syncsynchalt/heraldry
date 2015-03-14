e6fdf029fc0e6bb3ccfd279cad1067f44c1e9c67
Michael Driscoll <fenris@ulfheim.net>
2015-03-14 04:01:47 +0000

    Add polyfill from https://github.com/google/canvas-5-polyfill

--- device.js
+++ device.js
@@ -1,3 +1,17 @@
+// ellipse polyfill from canvas-5-polyfill
+// https://github.com/google/canvas-5-polyfill
+if (CanvasRenderingContext2D.prototype.ellipse == undefined) {
+  CanvasRenderingContext2D.prototype.ellipse = function(
+        x, y, radiusX, radiusY, rotation, startAngle, endAngle, antiClockwise) {
+    this.save();
+    this.translate(x, y);
+    this.rotate(rotation);
+    this.scale(radiusX, radiusY);
+    this.arc(0, 0, 1, startAngle, endAngle, antiClockwise);
+    this.restore();
+  }
+}
+
 var glob = {};
 
 function draw() {
